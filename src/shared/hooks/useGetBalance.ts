ce49f86490c9b585d47650f2de4ca5b4cf000bb8
mezger75 <9771804@gmail.com>
2025-02-10 18:59:54 +0300

    refactor: usdc contracts

--- src/shared/hooks/useGetBalance.ts
+++ src/shared/hooks/useGetBalance.ts
@@ -2,9 +2,8 @@ import { getBalance } from '@wagmi/core';
 import { Address, formatUnits } from 'viem';
 import { config } from '@/app/config';
 import { useQuery } from '@tanstack/react-query';
+import { USDC_ARB_SEPOLIA, USDC_CONTRACT_ADDRESS_MAINNET } from '@/shared/config/contracts';
 
-const USDC_ARB_SEPOLIA = '0x75faf114eafb1bdbe2f0316df893fd58ce46aa4d';
-const USDC_MAINNET = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48';
 const ARB_SEPOLIA_CHAIN_ID = 421614;
 
 const fetchBalance = async ({
@@ -17,7 +16,7 @@ const fetchBalance = async ({
     if (!address) return '0';
     const result = await getBalance(config, {
         address,
-        token: chainId === ARB_SEPOLIA_CHAIN_ID ? USDC_ARB_SEPOLIA : USDC_MAINNET, // 42164 is the Arbitrum Sepolia
+        token: chainId === ARB_SEPOLIA_CHAIN_ID ? USDC_ARB_SEPOLIA : USDC_CONTRACT_ADDRESS_MAINNET, // 42164 is the Arbitrum Sepolia
         chainId,
     });
 
